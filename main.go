ad0696c4c5a25d958fbcc5b0f34c3234d2a8089c
Mai Mouri <maimouri.jp@gmail.com>
2022-11-22 13:50:13 +0900

    main.go CRUD updated

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 
@@ -30,7 +27,7 @@ type Todo struct {
 
 // DBマイグレート
 func dbInit() {
-	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test")
+	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test?parseTime=true")
 	if err != nil {
 		panic("データベース開けず!（dbInit）")
 	}
@@ -40,7 +37,7 @@ func dbInit() {
 
 // DB追加
 func dbInsert(text string, status string) {
-	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test")
+	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test?parseTime=true")
 	if err != nil {
 		panic("データベース開けず！（dbInsert)")
 	}
@@ -50,7 +47,7 @@ func dbInsert(text string, status string) {
 
 // DB全取得
 func dbGetAll() []Todo {
-	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test")
+	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test?parseTime=true")
 	if err != nil {
 		panic("データベース開けず！(dbGetAll())")
 	}
@@ -62,7 +59,7 @@ func dbGetAll() []Todo {
 
 // DB一つ取得
 func dbGetOne(id int) Todo {
-	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test")
+	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test?parseTime=true")
 	if err != nil {
 		panic("データベース開けず！(dbGetOne())")
 	}
@@ -74,7 +71,7 @@ func dbGetOne(id int) Todo {
 
 // DB更新
 func dbUpdate(id int, text string, status string) {
-	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test")
+	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test?parseTime=true")
 	if err != nil {
 		panic("データベース開けず！（dbUpdate)")
 	}
@@ -88,7 +85,7 @@ func dbUpdate(id int, text string, status string) {
 
 // DB削除
 func dbDelete(id int) {
-	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test")
+	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test?parseTime=true")
 	if err != nil {
 		panic("データベース開けず！（dbDelete)")
 	}
@@ -99,7 +96,7 @@ func dbDelete(id int) {
 }
 
 func GetDBConnection() *gorm.DB {
-	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test")
+	db, err := gorm.Open("mysql", "tester:secret@tcp(db:3306)/test?parseTime=true")
 
 	if err != nil {
 		panic(err.Error())
@@ -107,49 +104,17 @@ func GetDBConnection() *gorm.DB {
 	return db
 }
 
-// func getUsers() []*User {
-func getUsers() []*User {
-	// Open up our database connection.
-	db, err := sql.Open("mysql", "tester:secret@tcp(db:3306)/test")
-
-	// if there is an error opening the connection, handle it
-	if err != nil {
-		log.Print(err.Error())
-	}
-	defer db.Close()
-
-	// Execute the query
-	results, err := db.Query("SELECT * FROM users")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-
-	var users []*User
-	for results.Next() {
-		var u User
-		// for each row, scan the result into our tag composite object
-		err = results.Scan(&u.ID, &u.Name)
-		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
-		}
-
-		users = append(users, &u)
-	}
-
-	return users
-}
-
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func userPage(w http.ResponseWriter, r *http.Request) {
-	users := getUsers()
+// func userPage(w http.ResponseWriter, r *http.Request) {
+// 	users := getUsers()
 
-	fmt.Println("Endpoint Hit: usersPage")
-	json.NewEncoder(w).Encode(users)
-}
+// 	fmt.Println("Endpoint Hit: usersPage")
+// 	json.NewEncoder(w).Encode(users)
+// }
 
 func main() {
 	r := gin.Default()
@@ -184,27 +149,28 @@ func main() {
 		ctx.HTML(200, "detail.html", gin.H{"todo": todo})
 	})
 
-	// 一覧
-	r.GET("/userlist", func(c *gin.Context) {
-		// users := dbGetAll()
-		users := getUsers()
-
-		if users != nil {
-			fmt.Printf("Cannot Load users!!")
+	//削除確認
+	r.GET("/delete_check/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
 		}
-
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"user": users,
-		})
+		todo := dbGetOne(id)
+		ctx.HTML(200, "delete.html", gin.H{"todo": todo})
 	})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	//Delete
+	r.POST("/delete/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		dbDelete(id)
+		ctx.Redirect(302, "/")
+
 	})
 
-	// http.HandleFunc("/users", userPage)
-	// log.Fatal(http.ListenAndServe(":8080", nil))
 	r.Run(":8080")
 }
